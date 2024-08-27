3ed01a504b9cc1a236edcaef1be752fb06caabc4
Ritik6559 <ritikjoshi741@gmail.com>
2024-08-27 19:40:40 +0530

    adding home screen

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text("Create/Join a room to play")
+        ],
+      ),
+    );
+  }
+}
