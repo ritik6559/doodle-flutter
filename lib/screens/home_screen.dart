18eba21a1b10ed306faa6d0f82972842f3db6539
Ritik6559 <ritikjoshi741@gmail.com>
2024-08-30 13:37:41 +0530

    home screen ui completed

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -10,12 +10,74 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       body: Column(
         mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.center,
         children: [
-          Text("Create/Join a room to play")
+          const Text(
+            "Create/Join a room to play",
+            style: TextStyle(
+              color: Colors.black,
+              fontSize: 24,
+            ),
+          ),
+          SizedBox(
+            height: MediaQuery.of(context).size.height * 0.1,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              ElevatedButton(
+                onPressed: () {},
+                style: ButtonStyle(
+                  backgroundColor: WidgetStateProperty.all(Colors.blue),
+                  textStyle: WidgetStateProperty.all(
+                    const TextStyle(
+                      color: Colors.white,
+                    ),
+                  ),
+                  minimumSize: WidgetStateProperty.all(
+                    Size(
+                      MediaQuery.of(context).size.width / 2.5,
+                      50,
+                    ),
+                  ),
+                ),
+                child: const Text(
+                  "Create",
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 16,
+                  ),
+                ),
+              ),
+              ElevatedButton(
+                onPressed: () {},
+                style: ButtonStyle(
+                  backgroundColor: WidgetStateProperty.all(Colors.blue),
+                  textStyle: WidgetStateProperty.all(
+                    const TextStyle(
+                      color: Colors.white,
+                    ),
+                  ),
+                  minimumSize: WidgetStateProperty.all(
+                    Size(
+                      MediaQuery.of(context).size.width / 2.5,
+                      50,
+                    ),
+                  ),
+                ),
+                child: const Text(
+                  "Join",
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 16,
+                  ),
+                ),
+              ),
+            ],
+          )
         ],
       ),
     );
