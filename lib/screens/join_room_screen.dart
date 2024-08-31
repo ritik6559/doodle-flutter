bb9f267f780352f5f733dcdb953fdc794d84ec70
Ritik6559 <ritikjoshi741@gmail.com>
2024-08-31 10:23:07 +0530

    completing join room screen ui

---
+++ lib/screens/join_room_screen.dart
@@ -0,0 +1,89 @@
+import 'package:doodle/widgets/custom_text_field.dart';
+import 'package:flutter/material.dart';
+
+class JoinRoomScreen extends StatefulWidget {
+  const JoinRoomScreen({super.key});
+
+  @override
+  State<JoinRoomScreen> createState() => _JoinRoomScreenState();
+}
+
+class _JoinRoomScreenState extends State<JoinRoomScreen> {
+  final TextEditingController _nameController = TextEditingController();
+  final TextEditingController _roomNameController = TextEditingController();
+
+  void joinRoom() {
+    if (_nameController.text.isNotEmpty &&
+        _roomNameController.text.isNotEmpty) {
+      Map<String, String> data = {
+        "nickname": _nameController.text,
+        "name": _roomNameController.text
+      };
+
+      Navigator.of(context).push(MaterialPageRoute(
+          builder: (context) =>
+              PaintScreen(data: data, screenFrom: 'joinRoom')));
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          const Text(
+            "Join Room",
+            style: TextStyle(
+              color: Colors.black,
+              fontSize: 30,
+            ),
+          ),
+          SizedBox(height: MediaQuery.of(context).size.height * 0.08),
+          Container(
+            margin: const EdgeInsets.symmetric(horizontal: 20),
+            child: CustomTextField(
+              controller: _nameController,
+              hintText: "Enter your name",
+            ),
+          ),
+          const SizedBox(height: 20),
+          Container(
+            margin: const EdgeInsets.symmetric(horizontal: 20),
+            child: CustomTextField(
+              controller: _roomNameController,
+              hintText: "Enter Room Name",
+            ),
+          ),
+          const SizedBox(height: 40),
+          ElevatedButton(
+            onPressed: joinRoom,
+            style: ButtonStyle(
+              backgroundColor: const WidgetStatePropertyAll(
+                Colors.blue,
+              ),
+              textStyle: const WidgetStatePropertyAll(
+                TextStyle(
+                  color: Colors.white,
+                ),
+              ),
+              minimumSize: WidgetStatePropertyAll(
+                Size(
+                  MediaQuery.of(context).size.width / 2.5,
+                  50,
+                ),
+              ),
+            ),
+            child: const Text(
+              "Join",
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 16,
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
