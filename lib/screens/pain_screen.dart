cb9b465f7b93733ba0de027053fb59fb53560bbd
Ritik6559 <ritikjoshi741@gmail.com>
2024-08-31 10:23:49 +0530

    adding paint screen

---
+++ lib/screens/pain_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class PainScreen extends StatefulWidget {
+  const PainScreen({super.key});
+
+  @override
+  State<PainScreen> createState() => _PainScreenState();
+}
+
+class _PainScreenState extends State<PainScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
