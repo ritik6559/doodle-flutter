8b4c0de7935782cf9f25c42f927b8c169823b8b9
Ritik6559 <ritikjoshi741@gmail.com>
2024-09-01 10:52:04 +0530

    adding sockets

--- server/index.js
+++ server/index.js
@@ -1,12 +1,21 @@
 const express = require('express');
 const mongodb = require('mongoose');
-
-const PORT = 3000;
+const http = require('http');
+const PORT = process.env.PORT || 3000;
 const app = express();
+const server = http.createServer(app);
+var io = require('socket.io')(server);
+
+//middleware
+app.use(express.json());
+
+
 const DB = "mongodb+srv://ritikjoshi741:9456597017ritik@cluster0.vvntg.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 
 mongodb.connect(DB).then(() =>{
     console.log("connected successfully");
+}).catch((e) => {
+    console.log(e);
 })
 
 app.listen(PORT, '0.0.0.0',() => {
